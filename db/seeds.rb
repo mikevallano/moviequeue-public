022a650ca675fa90efb26547fba346650017f28a
anne <richardson.ae@gmail.com>
2015-07-06 15:17:49 -0500

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,9 @@ Movie.create(title: "Fargo", imdb_url: "http://www.imdb.com/title/tt0116282/", w
 
 Movie.create(title: "Goonies", imdb_url: "http://www.imdb.com/title/tt0089218", watch_url: "http://www.dailymotion.com/video/x2f41uy", date_watched: 19870608061017, location_watched: "Finley's House", category_id: 4, watchlist_id: 1, our_rating: 5, notes: "One of my all time favorite movies.", imdb_artwork: "http://ia.media-imdb.com/images/M/MV5BMTEyMzM3NDQyMjJeQTJeQWpwZ15BbWU4MDE4ODY0NjEx._V1_SX214_AL_.jpg", imdb_plot_summary: "In order to save their home from foreclosure, a group of misfits set out to find a pirate's ancient treasure.", imdb_rating: 7.8, imdb_genre: "Adventure, Comedy, Family")
 # :preview_link "http://www.imdb.com/video/screenplay/vi2118099993/", :runtime 114
+
+Movie.create(title: "Starsky & Hutch", imdb_url: "http://www.imdb.com/title/tt0335438", watch_url: "http://www.amazon.com/Starsky-Hutch-Ben-Stiller/dp/B001QLMI98", date_watched: 19870608061017, location_watched: "Everywhere", category_id: 3, watchlist_id: 3, our_rating: 4, notes: "One of my all time favorite movies.", imdb_artwork: "x", imdb_plot_summary: "Two streetwise cops bust criminals in their red-and-white Ford Torino with the help of police snitch called Huggy Bear.", imdb_rating: 6.1, imdb_genre: "Comedy")
+
+Movie.create(title: "x", imdb_url: "x", watch_url: "x", date_watched: 19870608061017, location_watched: "x", category_id: 3, watchlist_id: 3, our_rating: 0, notes: "One of my all time favorite movies.", imdb_artwork: "artwork-blank.jpg", imdb_plot_summary: "x", imdb_rating: 7.8, imdb_genre: "x")
+
+# Movie.create(title: "x", imdb_url: "x", watch_url: "x", date_watched: 19870608061017, location_watched: "x", category_id: 3, watchlist_id: 3, our_rating: 0, notes: "One of my all time favorite movies.", imdb_artwork: "x", imdb_plot_summary: "x", imdb_rating: 7.8, imdb_genre: "x")
