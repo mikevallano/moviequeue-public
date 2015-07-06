f818d0ce2aaa3fc343a3780efd127ea688be22be
anne <richardson.ae@gmail.com>
2015-07-06 13:04:53 -0500

    initial db migration and setup

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,3 +2,30 @@ class Movie < ActiveRecord::Base
   belongs_to :category
   belongs_to :watchlist
 end
+
+# == Schema Information
+#
+# Table name: movies
+#
+#  id                :integer          not null, primary key
+#  title             :string
+#  imdb_url          :string
+#  watch_url         :string
+#  date_watched      :date
+#  location_watched  :string
+#  category_id       :integer
+#  watchlist_id      :integer
+#  our_rating        :float
+#  notes             :text
+#  imdb_artwork      :string
+#  imdb_plot_summary :text
+#  imdb_rating       :float
+#  imdb_genre        :text
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_movies_on_category_id   (category_id)
+#  index_movies_on_watchlist_id  (watchlist_id)
+#
--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,2 +1,12 @@
 class Watchlist < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: watchlists
+#
+#  id         :integer          not null, primary key
+#  list       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
