04c79490565b605c9fae36cd4f41a6380c29f34c
anne <richardson.ae@gmail.com>
2015-07-17 16:10:49 -0500

    added header back to modal and new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,21 @@
 
 Watchlist.create(list: "Anne")
 Watchlist.create(list: "Mike")
-Watchlist.create(list: "Both")
+Watchlist.create(list: "Both of Us")
 
 Category.create(name: "Hunker In")
 Category.create(name: "Christmas")
 Category.create(name: "Old Standby")
 Category.create(name: "G00d One")
-Category.create(name: "Weeknight")
+Category.create(name: "Weeknighter")
 Category.create(name: "Comfort Blanket")
-Category.create(name: "Indie")
-Category.create(name: "Marathon")
+Category.create(name: "Indie Flick")
+Category.create(name: "Silly Comedy")
+Category.create(name: "RomCom")
+Category.create(name: "Documentary")
+Category.create(name: "Nature Program")
+Category.create(name: "G00d Drama")
+Category.create(name: "JasonStathamPunchFest") 
 
 Movie.create(title: "Fargo", imdb_url: "http://www.imdb.com/title/tt0116282/", watch_url: "http://www.amazon.com/Fargo-William-H-Macy/dp/B0030MDS36", date_watched: Date.new(2996, 7, 8), location_watched: "Isla Mujeres, MX", category_id: 4, watchlist_id: 2, our_rating: 5, notes: "One of my all time favorite movies.", imdb_artwork: "http://ia.media-imdb.com/images/M/MV5BMTgxNzY3MzUxOV5BMl5BanBnXkFtZTcwMDA0NjMyNA@@._V1_SX214_AL_.jpg", imdb_plot_summary: "Jerry Lundegaard's inept crime falls apart due to his and his henchmen's bungling and the persistent police work of the quite pregnant Marge Gunderson.", imdb_rating: 8.5, imdb_genre: "Crime, Drama, Thrller")
 # :preview_link "http://www.imdb.com/video/screenplay/vi2228618777", :runtime 98
