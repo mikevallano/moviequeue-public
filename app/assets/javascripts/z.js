29d3acddb2e27e7bf031ecdde5547d83fc3b7399
mike vallano <mikevallano@gmail.com>
2015-07-22 13:13:38 -0500

    Closer to full ajax pagination

--- app/assets/javascripts/z.js
+++ app/assets/javascripts/z.js
@@ -13,16 +13,33 @@ $(document).ready(function() {
 
 
 // ajax pagination
+
+var current_unwatched_url = "http://localhost:3000/movies.js";
+var current_watched_url = "http://localhost:3000/movies.js";
+
   $('#unwatched_movies_div').on("click", ".pagination a", function () {
     $.getScript(this.href);
+    current_unwatched_url = this.href;
+    console.log("current unwatched url do be: " + current_unwatched_url + "  current watched URL do be: " + current_watched_url);
     return false;
   });
 
-
-  // $('#watched_movies_div').on("click", ".pagination a", function () {
-  //   $.getScript(this.href);
-  //   return false;
+  // $('#unwatched_movies_div').on("click", ".pagination a", function () {
+  //   current_unwatched_url = this.href;
+  //     $.ajax({
+  //       type: 'GET',
+  //       url: current_watched_url,
+  //       dataType: 'script',
+  //       return false;
+  //     });
   // });
+
+  $('#watched_movies_div').on("click", ".pagination a", function () {
+    $.getScript(this.href);
+    current_watched_url = this.href;
+    console.log("current unwatched url do be: " + current_unwatched_url + "  current watched URL do be: " + current_watched_url);
+    return false;
+  });
 
 
 
